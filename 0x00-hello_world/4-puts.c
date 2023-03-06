1fad49b866bbc4de1a7d0a9e3869ae6b356367d0
Ernest1229 <mensahkafui@gmial.com>
2023-03-06 13:57:18 +0000

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/* C programming using puts */
+/* C programming using puts with function main */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
