3a6421d51ef1ee2cd53180d14d9385229e348788
Ernest1229 <mensahkafui@gmial.com>
2023-06-18 02:58:26 +0000

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,14 +2,13 @@
 /**
  *main -  Entry level of the program
  *No parameter invloved
- * 
- * Long Description: with proper grammar, but the outcome is a piece of art,
- * Return: 0
+ *
+ *Long Description: with proper grammar, but the outcome is a piece of art,
+ *Return: 0
  */
- 
+
 int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,");
-
-    return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
