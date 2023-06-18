8dd86e01f4d5f6d11d41e334946c8d4ab4811b66
Ernest1229 <mensahkafui@gmial.com>
2023-06-18 04:41:22 +0000

    8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+  *main - Entry Point
+  *No parameter
+  *
+  *Description : printing
+  * Return : 1
+  */
+int main(void)
+{
+	char(word) = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	putchar(word);
+	return (1);
+}
