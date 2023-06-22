2139e49644015ad73a735c843cbb6373f40937e0
Ernest1229 <mensahkafui@gmial.com>
2023-06-22 00:45:45 +0000

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+  *main-Entry point
+  *parameter-alpha
+  *
+  *Description-Code to print a to z
+  *Return-0
+  */
+
+main()
+{
+	int alpha;
+
+	alpha = 97;
+	for (alpha = 97; alpha = 122; ++alpha)
+	{
+	putchar(alpha);
+	}
+}
