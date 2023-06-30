751de5640821c527c5fa8d5a1c7ac1d52be8eb1d
Ernest1229 <mensahkafui@gmial.com>
2023-06-30 05:10:25 +0000

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  *print_last_digit(int) - prints last digit
+  *
+  *return: last digit
+  */
+
+int print_last_digit(int)
+{
+	int r;
+
+	r / 10 = j;
+
+	s = _putchar(".1%f", j);
+
+	return (s);
+
+
+}
