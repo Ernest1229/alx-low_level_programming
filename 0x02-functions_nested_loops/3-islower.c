41d5f3ce794169211e3c71f3418afbace8f77fba
Ernest1229 <mensahkafui@gmial.com>
2023-06-30 03:20:57 +0000

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  *int _islower(int c) - lowercase
+  *Use _putchar
+  *
+  */
+
+int _islower(int c)
+{
+	int c;
+
+	if (c <= 97 && c >= 122)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+
+}
