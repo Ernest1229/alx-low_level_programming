3aa8fd76acc53a3c30a957d2b9e8e409a210ba03
Ernest1229 <mensahkafui@gmial.com>
2023-07-01 22:39:54 +0000

    Times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,20 +15,21 @@ void times_table(void)
 
 	for (a = 0; a <= 9; a++)
 	{
+	_putchar('0');
 	for (b = 1; b <= 9; b++)
-	{
-	_putchar('0');	
-	c = a * b;
+	{	
 	_putchar(',');
 	_putchar(' ');
-
+	c = a * b;
 	if (c <= 9)
+	{
 	_putchar(' ');
+	}
 	else
 	{
 	_putchar((c / 10) + '0');
-	_putchar((c % 10) + '0');
 	}
+	_putchar((c % 10) + '0');
 	}
 	_putchar('\n');
 	}
