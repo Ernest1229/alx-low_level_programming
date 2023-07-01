8283183b55740e6d6a1f25d5cbcd5980855ef37b
Ernest1229 <mensahkafui@gmial.com>
2023-07-01 01:20:46 +0000

    Times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+  *times_table - prints times table
+  *@a @b @c: are integer parameters
+  *
+  *Return: 0
+  */
+
+void times_table(void)
+{
+	int a;
+	int b;
+	int c;
+
+	for (a = 0; a <= 9; a++)
+	{
+	for (b = 0; b <= 9; b++)
+	{
+	c = a * b;
+	_putchar(c + '0');
+	_putchar(',');
+	_putchar(' ');
+	if (c <= 9)
+	{
+	_putchar(' ');
+	}
+	}
+	_putchar('\n');
+	}
+}
