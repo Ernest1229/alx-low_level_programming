71b649a091ca60e81b1a6c0c88f8985fadd0bdd3
Ernest1229 <mensahkafui@gmial.com>
2023-07-01 02:58:40 +0000

    Times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdlib.h>
+
 /**
   *times_table - prints times table
   *@a @b @c: are integer parameters
@@ -15,15 +15,19 @@ void times_table(void)
 
 	for (a = 0; a <= 9; a++)
 	{
+	_putchar('0');
 	for (b = 0; b <= 9; b++)
 	{
 	c = a * b;
-	_putchar(c + '0');
 	_putchar(',');
 	_putchar(' ');
+
 	if (c <= 9)
-	{
 	_putchar(' ');
+	else
+	{
+	_putchar(c / 10 + '0');
+	_putchar(c % 10 + '0');
 	}
 	}
 	_putchar('\n');
