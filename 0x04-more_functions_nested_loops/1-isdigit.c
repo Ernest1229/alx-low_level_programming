94b66f48c2c4b9ff411380baf0dac058c3203696
Ernest1229 <mensahkafui@gmial.com>
2023-07-02 13:38:15 +0000

    isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-*_isupper - checks for uppercase character
+*_isdigit - checks for uppercase character
 *@c : integer parameter
 *
 *Return: 1 if uppercase and 0 if otherwise
