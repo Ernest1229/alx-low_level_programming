f7fbe4d3fffab5d35e92ffb2f30a43d36fbe9ff9
Ernest1229 <mensahkafui@gmial.com>
2023-07-03 22:12:03 +0000

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+  *_strspn - gets the length of a prefix substring
+  *@s: string from initial segment
+  *@accept: string from accepted segment
+  *
+  *Return: s - number of bytes
+  */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int a;
+	unsigned int b;
+	unsigned int c;
+
+	a = 0;
+	t = 0;
+
+	while (accept[a])
+	{
+	b = 0;
+
+	while (s[b] != 32)
+	{
+	if (accept[a] == s[b])
+	{
+	c++;
+	}
+	b++;
+	}
+	a++;
+	}
+	return (c);
+}
