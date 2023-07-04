12ae6414705d61fde449668e962c152ec8997d1c
Ernest1229 <mensahkafui@gmial.com>
2023-07-04 09:47:15 +0000

    reverse gear

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+  *print_rev - prints a string in reverse
+  *@: string
+  *
+  *Return: 0
+  */
+
+void print_rev(char *s)
+{
+	int a;
+
+	a = 0;
+
+	while (s[a])
+	{
+		a++;
+	}
+
+	a--;
+
+	for (; a >= 0; a--)
+	{
+		_putchar(s[a]);
+	}
+
+	_putchar('\n');
+}
